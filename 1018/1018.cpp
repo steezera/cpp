c097633512866d155602330a94ce01ef95f2521e
Jihun Song <wlgns1080@naver.com>
2023-12-29 02:27:19 +0900

    Update 1018.cpp

--- 1018/1018.cpp
+++ 1018/1018.cpp
@@ -3,15 +3,10 @@
 #include <algorithm>
 #include <math.h>
 #include <string>
-
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-
-    int N,M;
-    string white[8] = {
+string board[50];
+string white[8] = {
         "WBWBWBWB",
         "BWBWBWBW",
         "WBWBWBWB",
@@ -32,7 +27,43 @@ int main() {
         "BWBWBWBW",
         "WBWBWBWB"
     };
+int countBW(int x, int y) {
+    int cnt = 0;
+    for(int i = 0; i < 8; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            if(board[i + x][j + y] != black[i][j]) cnt += 1;
+        }
+    }
+    return cnt;
+}
 
-    
+int countWB(int x, int y) {
+    int cnt = 0;
+    for(int i = 0; i < 8; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            if(board[i + x][j + y] != white[i][j]) cnt += 1;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int minVal = 10000; // 충분히 큰 숫자
+    int N,M;
+    cin >> N >> M;
+    for(int i = 0; i < N; ++i) {
+        getline(cin, board[i]);
+    }
+
+    for(int i = 8; i < N; ++i){
+        for(int j = 8; j < M; ++j) {
+            int tmp = min(countWB(i, j), countBW(i, j));
+            if(tmp < minVal) minVal = tmp;
+        }
+    }
+    cout << minVal;
     return 0;
 }
