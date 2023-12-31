bec46f3e4becd07a9944d74b69a65ee9b5b6dfff
Jihun Song <wlgns1080@naver.com>
2023-12-31 14:34:11 +0900

    fix 1018(for loop range)
    fix for loop range

--- 1018/1018.cpp
+++ 1018/1018.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <math.h>
-#include <string>
+#include <limits.h> // INT_MAX를 사용하기 위한 헤더
 using namespace std;
 
 string board[50];
 string white[8] = {
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-    };
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+};
 
-    string black[8] = {
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB",
-        "BWBWBWBW",
-        "WBWBWBWB"
-    };
+string black[8] = {
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+};
+
 int countBW(int x, int y) {
     int cnt = 0;
-    for(int i = 0; i < 8; ++i) {
-        for(int j = 0; j < 8; ++j) {
-            if(board[i + x][j + y] != black[i][j]) cnt += 1;
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if (board[i + x][j + y] != black[i][j]) cnt += 1;
         }
     }
     return cnt;
@@ -39,9 +39,9 @@ int countBW(int x, int y) {
 
 int countWB(int x, int y) {
     int cnt = 0;
-    for(int i = 0; i < 8; ++i) {
-        for(int j = 0; j < 8; ++j) {
-            if(board[i + x][j + y] != white[i][j]) cnt += 1;
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if (board[i + x][j + y] != white[i][j]) cnt += 1;
         }
     }
     return cnt;
@@ -51,17 +51,19 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int minVal = 10000; // 충분히 큰 숫자
-    int N,M;
+    int minVal = INT_MAX; // 충분히 큰 숫자
+    int N, M;
     cin >> N >> M;
-    for(int i = 0; i < N; ++i) {
-        getline(cin, board[i]);
+    cin.ignore(); // 개행 문자 처리
+
+    for (int i = 0; i < N; ++i) {
+        cin >> board[i];
     }
 
-    for(int i = 8; i < N; ++i){
-        for(int j = 8; j < M; ++j) {
+    for (int i = 0; i <= N - 8; ++i) {
+        for (int j = 0; j <= M - 8; ++j) {
             int tmp = min(countWB(i, j), countBW(i, j));
-            if(tmp < minVal) minVal = tmp;
+            if (tmp < minVal) minVal = tmp;
         }
     }
     cout << minVal;
